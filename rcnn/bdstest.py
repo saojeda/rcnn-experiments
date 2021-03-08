4fe1fa0374905fec06a74353c3807f74a10d29ee
Sun Arthur Ojeda <saojeda@up.edu.ph>
2021-03-08 10:21:15 +0800

    Update bdstest.py
    Replace file path with placeholder path

--- rcnn/bdstest.py
+++ rcnn/bdstest.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
     for name in regression_datasets:
         print(name)
-        dataloader = CNNDataLoader(path_to_file="/home/sun/Documents/Monash_UEA_UCR_Regression_Archive/"
+        dataloader = CNNDataLoader(path_to_file="/path/to/Monash_UEA_UCR_Regression_Archive/"
                                                 f"{name}/{name}_TEST.ts", img_size=4)
         residuals = dataloader.get_residuals()
         for i in range(residuals.shape[0]):
